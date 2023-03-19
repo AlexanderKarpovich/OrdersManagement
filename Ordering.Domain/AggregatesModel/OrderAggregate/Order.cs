347b73dadc9d3f029ff3776d573f991023f6cc13
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-03-19 17:31:13 +0300

    Order model added.

---
+++ Ordering.Domain/AggregatesModel/OrderAggregate/Order.cs
@@ -0,0 +1,55 @@
+﻿namespace Ordering.Domain.AggregatesModel.OrderAggregate
+{
+    public class Order : Entity, IAggregateRoot
+    {
+        private string? number;
+        private DateTime date;
+        private int providerId;
+        private readonly List<OrderItem> orderItems;
+
+        public string? Number => number;
+        public DateTime Date => date;
+        public int ProviderId => providerId;
+
+        public IReadOnlyCollection<OrderItem> OrderItems => orderItems;
+
+        protected Order()
+        {
+            orderItems = new List<OrderItem>();
+        }
+
+        public Order(string number, DateTime date, int providerId) : this()
+        {
+            this.number = number;
+            this.date = date;
+            this.providerId = providerId;
+        }
+
+        public void UpdateOrder(string number, DateTime date, int providerId, IEnumerable<OrderItem>? items = null)
+        {
+            this.number = number;
+            this.date = date;
+            this.providerId = providerId;
+
+            if (items is not null)
+            {
+                orderItems.Clear();
+                foreach (OrderItem item in items)
+                {
+                    orderItems.Add(item);
+                }
+            }
+        }
+
+        public void ClearOrderItems()
+        {
+            orderItems.Clear();
+        }
+
+        public void AddOrderItem(string name, decimal quantity, string unit)
+        {
+            var orderItem = new OrderItem(name, quantity, unit);
+            orderItems.Add(orderItem);
+        }
+    }
+}
