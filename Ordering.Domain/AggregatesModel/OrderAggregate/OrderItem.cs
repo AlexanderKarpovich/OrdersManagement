3786e58f84d9141e67cb0c20fad1173c9b2104a0
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-03-19 17:31:03 +0300

    Order item model added.

---
+++ Ordering.Domain/AggregatesModel/OrderAggregate/OrderItem.cs
@@ -0,0 +1,28 @@
+﻿namespace Ordering.Domain.AggregatesModel.OrderAggregate
+{
+    public class OrderItem : Entity
+    {
+        private string? name;
+        private decimal quantity;
+        private string? unit;
+        private int orderId;
+
+        public OrderItem() { }
+
+        public OrderItem(string name, decimal quantity, string unit)
+        {
+            this.name = name;
+            this.quantity = quantity;
+            this.unit = unit;
+            orderId = 0;
+        }
+
+        public string? Name => name;
+        
+        public decimal Quantity => quantity;
+
+        public string? Unit => unit;
+
+        public int OrderId => orderId;
+    }
+}
