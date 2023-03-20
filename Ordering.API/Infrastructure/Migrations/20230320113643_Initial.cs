4b4d9fe72e24b2d3f50966e82a469a1c9cb8f74d
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-03-20 15:07:25 +0300

    Database initial migration added.

---
+++ Ordering.API/Infrastructure/Migrations/20230320113643_Initial.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ordering.API.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "ordering");
+
+            migrationBuilder.CreateSequence(
+                name: "orderitemseq",
+                schema: "ordering",
+                incrementBy: 10);
+
+            migrationBuilder.CreateSequence(
+                name: "orderseq",
+                schema: "ordering",
+                incrementBy: 10);
+
+            migrationBuilder.CreateSequence(
+                name: "providerseq",
+                schema: "ordering",
+                incrementBy: 10);
+
+            migrationBuilder.CreateTable(
+                name: "providers",
+                schema: "ordering",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_providers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "requests",
+                schema: "ordering",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Time = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_requests", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orders",
+                schema: "ordering",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2(7)", nullable: false),
+                    Number = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProviderId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_orders_providers_ProviderId",
+                        column: x => x.ProviderId,
+                        principalSchema: "ordering",
+                        principalTable: "providers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orderItems",
+                schema: "ordering",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<decimal>(type: "decimal(18,3)", precision: 18, scale: 3, nullable: false),
+                    Unit = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_orderItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_orderItems_orders_OrderId",
+                        column: x => x.OrderId,
+                        principalSchema: "ordering",
+                        principalTable: "orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orderItems_OrderId",
+                schema: "ordering",
+                table: "orderItems",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orders_ProviderId",
+                schema: "ordering",
+                table: "orders",
+                column: "ProviderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "orderItems",
+                schema: "ordering");
+
+            migrationBuilder.DropTable(
+                name: "requests",
+                schema: "ordering");
+
+            migrationBuilder.DropTable(
+                name: "orders",
+                schema: "ordering");
+
+            migrationBuilder.DropTable(
+                name: "providers",
+                schema: "ordering");
+
+            migrationBuilder.DropSequence(
+                name: "orderitemseq",
+                schema: "ordering");
+
+            migrationBuilder.DropSequence(
+                name: "orderseq",
+                schema: "ordering");
+
+            migrationBuilder.DropSequence(
+                name: "providerseq",
+                schema: "ordering");
+        }
+    }
+}
