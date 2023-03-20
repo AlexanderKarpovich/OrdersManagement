fab1b36a16891827c7662cb92320df4b44225d72
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-03-20 15:07:05 +0300

    Services and application configured.

---
+++ Ordering.API/Program.cs
@@ -0,0 +1,59 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Configuring DI service collection
+builder.Services
+    .AddCustomControllers()
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen()
+    .AddCustomHealthChecks(builder.Configuration)
+    .AddCustomDbContext(builder.Configuration)
+    .AddCustomConfiguration();
+
+// Configure Autofac as the service provider
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+    .ConfigureContainer<ContainerBuilder>(containerBuilder =>
+    {
+        containerBuilder.RegisterMediatR(typeof(Program).Assembly);
+        containerBuilder.RegisterModule(new MediatorModule());
+        containerBuilder.RegisterModule(new ApplicationModule(
+            builder.Configuration.GetConnectionString("OrderingConnection") ?? ""));
+    });
+
+// Configuring Serilog logger
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File("../Logs/api-log-.txt", rollingInterval: RollingInterval.Hour)
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+// Using Serilog as logging provider
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapHealthChecks("/hc", new HealthCheckOptions()
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+app.MapHealthChecks("/liveness", new HealthCheckOptions()
+{
+    Predicate = r => r.Name.Contains("self")
+});
+
+await OrderingContextPrepare.EnsurePopulated(app);
+
+app.Run();
+
