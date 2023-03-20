ffde8fe666486348b8dd3ec65e49b3322cd48f21
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-03-20 15:06:41 +0300

    Extension class for IServiceCollection container added.

---
+++ Ordering.API/ServiceCollectionExtensions.cs
@@ -0,0 +1,66 @@
+﻿namespace Ordering.API
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCustomControllers(this IServiceCollection services)
+        {
+            services.AddControllers(options =>
+            {
+                options.Filters.Add(typeof(HttpGlobalExceptionFilter));
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddCustomHealthChecks(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHealthChecks()
+                .AddCheck("self", () => HealthCheckResult.Healthy())
+                .AddSqlServer(
+                    configuration.GetConnectionString("OrderingConnection") ?? "",
+                    name: "OrderingDatabase-check",
+                    tags: new string[] { "orderingdatabase" });
+
+            return services;
+        }
+
+        public static IServiceCollection AddCustomDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OrderingContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("OrderingConnection"), sqlOptions =>
+                {
+                    sqlOptions.MigrationsAssembly(typeof(Program).GetTypeInfo().Assembly.GetName().Name);
+                    sqlOptions.EnableRetryOnFailure(10, TimeSpan.FromSeconds(15), null);
+                });
+                options.LogTo(Log.Logger.Information, LogLevel.Information);
+            }, ServiceLifetime.Scoped);
+
+            return services;
+        }
+
+        public static IServiceCollection AddCustomConfiguration(this IServiceCollection services)
+        {
+            services.AddOptions();
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = context =>
+                {
+                    var problemDetails = new ValidationProblemDetails(context.ModelState)
+                    {
+                        Instance = context.HttpContext.Request.Path,
+                        Status = StatusCodes.Status400BadRequest,
+                        Detail = "Refer to the errors property to see additional details"
+                    };
+
+                    return new BadRequestObjectResult(problemDetails)
+                    {
+                        ContentTypes = { "application/problem+json", "application/problem+xml" }
+                    };
+                };
+            });
+
+            return services;
+        }
+    }
+}
